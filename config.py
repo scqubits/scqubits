c346ed7b90a9ff7e06a5a95a35ded52841202c97
Jens Koch <jens-koch@northwestern.edu>
2016-08-24 19:09:42 -0500

    python code containing all globals/settings

---
+++ config.py
@@ -0,0 +1,18 @@
+# config.py
+
+import numpy as np
+
+EVECS_FILESUFFIX = '_evecs'
+EVALS_FILESUFFIX = '_evals'
+PARAMETER_FILESUFFIX = '.prm'
+
+FILE_FORMAT = 'h5'   # choose 'csv' instead for generation of comma-separated values files
+
+MODE_FUNC_DICT = {'abs_sqr': (lambda x: np.abs(x)**2),
+                  'abs': (lambda x: np.abs(x)),
+                  'real': (lambda x: np.real(x)),
+                  'imag': (lambda x: np.imag(x))}
+
+PHI_INDEX = 0
+THETA_INDEX = 1
+CHI_INDEX = 2
