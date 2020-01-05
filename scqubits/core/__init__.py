f121526a30c6013a9094ee7d41763397805da4c6
Jens Koch <jens-koch@northwestern.edu>
2020-01-04 21:22:57 -0600

    docstring inserted

--- scqubits/core/__init__.py
+++ scqubits/core/__init__.py
@@ -5,8 +5,7 @@
 #
 #     This source code is licensed under the BSD-style license found in the
 #     LICENSE file in the root directory of this source tree.
-"""scqubits.utils contains utility modules for plotting, file input/output, constants used throughout the package \
-and other routines."""
+"""scqubits.core contains the central parts of the scqubits package, including implementations of individual \
+superconducting qubits, classes for storing and manipulating spectral data, for composing joint Hilbert spaces \
+of multiple quantum systems, and routines for sweeping an external parameter for a given Hilbert space ."""
 #######################################################################################################################
-
-
