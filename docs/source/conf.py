1a8d45e32ab767af3224504759c5449f452c5c08
Jens Koch <jens-koch@northwestern.edu>
2020-01-20 08:39:17 -0600

    RTD failing again; defaulting to old sphinx despite pinning ?!! switching to py3.7 out of desperation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -59,7 +59,7 @@ exclude_patterns = ['_build', '**.ipynb_checkpoints']
 # -- Options for HTML output -------------------------------------------------
 
 # These folders are copied to the documentation's HTML output
-html_static_path = ['_static']
+# html_static_path = ['_static']
 
 # These paths are either relative to html_static_path
 # or fully qualified paths (eg. https://...)
