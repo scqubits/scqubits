5058fcb955ae6ab2e42757780c0132c5db91be57
dkweiss31 <dkweiss@u.northwestern.edu>
2020-04-29 15:57:39 -0500

    I added a comment!

--- scqubits/core/current_mirror_vchos.py
+++ scqubits/core/current_mirror_vchos.py
@@ -15,21 +15,21 @@ from scqubits.core.vchos import VCHOS
 from scqubits.core.storage import WaveFunctionOnGrid
 from scqubits.utils.spectrum_utils import standardize_phases, order_eigensystem
 
-
-#-Flux Qubit using VCHOS 
+#This is a new comment
+#-Flux Qubit using VCHOS
 
 class CurrentMirrorVCHOS(VCHOS):
-    def __init__(self, N, ECB, ECJ, ECg, EJlist, nglist, flux, 
+    def __init__(self, N, ECB, ECJ, ECg, EJlist, nglist, flux,
                  kmax, num_exc, squeezing=False, truncated_dim=None):
         self.num_big_cap = N
         self.ECB = ECB
         self.ECJ = ECJ
         self.ECg = ECg
         self.EJlist = EJlist
-        
+
         V_m = self._build_V_m()
         self.nglist = np.dot(sp.linalg.inv(V_m).T, nglist)[0:-1]
-        
+
         self.flux = flux
         self.kmax = kmax
         self.num_exc = num_exc
@@ -40,17 +40,17 @@ class CurrentMirrorVCHOS(VCHOS):
         self.Phi0 = 1. / (2*self.e)
         self.boundary_coeffs = np.ones(2*N-1)
         self.num_deg_freedom = 2*N - 1
-        
+
         self._evec_dtype = np.complex_
         self._default_grid = Grid1d(-6.5*np.pi, 6.5*np.pi, 651)
         self.truncated_dim = truncated_dim
-    
+
     def build_capacitance_matrix(self):
         N = self.num_big_cap
         CB = self.e**2 / (2.*self.ECB)
         CJ = self.e**2 / (2.*self.ECJ)
         Cg = self.e**2 / (2.*self.ECg)
-        
+
         Cmat = np.diagflat(
             [Cg + 2 * CJ + CB for j in range(2 * N)], 0)
         Cmat += np.diagflat([- CJ for j in range(2 * N - 1)], +1)
@@ -58,33 +58,33 @@ class CurrentMirrorVCHOS(VCHOS):
         Cmat += np.diagflat([- CB for j in range(N)], +N)
         Cmat += np.diagflat([- CB for j in range(N)], -N)
         Cmat[0, -1] = Cmat[-1, 0] = - CJ
-        
+
         V_m_inv = sp.linalg.inv(self._build_V_m())
         Cmat = np.matmul(V_m_inv.T, np.matmul(Cmat, V_m_inv))
-        
+
         return Cmat[0:-1, 0:-1]
-    
+
     def _build_V_m(self):
         N = self.num_big_cap
         V_m = np.diagflat([-1 for j in range(2*N)], 0)
         V_m += np.diagflat([1 for j in range(2*N - 1)], 1)
         V_m[-1] = np.array([1 for j in range(2*N)])
-        
+
         return V_m
-    
+
     def build_EC_matrix(self):
         """Return the charging energy matrix"""
         Cmat = self.build_capacitance_matrix()
         return 0.5 * self.e**2 * sp.linalg.inv(Cmat)
-    
+
     def hilbertdim(self):
         """Return N if the size of the Hamiltonian matrix is NxN"""
         return len(self.sorted_minima())*(self.num_exc+1)**(2*self.num_big_cap - 1)
-    
+
     def _check_if_new_minima(self, new_minima, minima_holder):
         """
         Helper function for find_minima, checking if new_minima is
-        indeed a minimum and is already represented in minima_holder. If so, 
+        indeed a minimum and is already represented in minima_holder. If so,
         _check_if_new_minima returns False.
         """
         if -self.potential(new_minima) <= 0: #maximum or saddle point then, not a minimum
@@ -96,13 +96,13 @@ class CurrentMirrorVCHOS(VCHOS):
             elem_bool = True
             for elem in diff_array_reduced:
                 # if every element is zero or 2pi, then we have a repeated minima
-                elem_bool = elem_bool and (np.allclose(elem,0.0,atol=1e-3) 
+                elem_bool = elem_bool and (np.allclose(elem,0.0,atol=1e-3)
                                            or np.allclose(elem,2*np.pi,atol=1e-3))
             if elem_bool:
                 new_minima_bool = False
                 break
         return new_minima_bool
-    
+
     def find_minima(self):
         """
         Index all minima
@@ -116,22 +116,22 @@ class CurrentMirrorVCHOS(VCHOS):
             result_neg = minimize(self.potential, guess_neg)
             new_minimum_pos = self._check_if_new_minima(result_pos.x, minima_holder)
             if new_minimum_pos:
-                minima_holder.append(np.array([np.mod(elem,2*np.pi) 
+                minima_holder.append(np.array([np.mod(elem,2*np.pi)
                                                for elem in result_pos.x]))
             new_minimum_neg = self._check_if_new_minima(result_neg.x, minima_holder)
             if new_minimum_neg:
-                minima_holder.append(np.array([np.mod(elem,2*np.pi) 
+                minima_holder.append(np.array([np.mod(elem,2*np.pi)
                                                for elem in result_neg.x]))
         return(minima_holder)
-    
+
     def sorted_minima(self):
         """Sort the minima based on the value of the potential at the minima """
         minima_holder = self.find_minima()
-        value_of_potential = np.array([self.potential(minima_holder[x]) 
+        value_of_potential = np.array([self.potential(minima_holder[x])
                                        for x in range(len(minima_holder))])
-        sorted_value_holder = np.array([x for x,_ in 
+        sorted_value_holder = np.array([x for x,_ in
                                          sorted(zip(value_of_potential, minima_holder), key=lambda x: x[0])])
-        sorted_minima_holder = np.array([x for _, x in 
+        sorted_minima_holder = np.array([x for _, x in
                                          sorted(zip(value_of_potential, minima_holder) , key=lambda x: x[0])])
         # For efficiency purposes, don't want to displace states into minima
         # that are too high energy. Arbitrarily set a 40 GHz cutoff
