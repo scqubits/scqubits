1256b7cb296202b5a51056067efc129b7d1e62e3
jkochNU <jens-koch@northwestern.edu>
2020-08-17 20:21:33 -0500

    win azure pipelines failure: testing whether isolated to FluxQubit

--- scqubits/tests/test_fluxqubit.py
+++ scqubits/tests/test_fluxqubit.py
@@ -16,13 +16,13 @@ from scqubits import FluxQubit
 from scqubits.tests.conftest import StandardTests
 
 
-class TestFluxQubit(StandardTests):
-    @classmethod
-    def setup_class(cls):
-        cls.qbt = None
-        cls.qbt_type = FluxQubit
-        cls.file_str = 'fluxqubit'
-        cls.op1_str = 'n_1_operator'
-        cls.op2_str = 'n_2_operator'
-        cls.param_name = 'flux'
-        cls.param_list = np.linspace(0.45, 0.55, 50)
+# class TestFluxQubit(StandardTests):
+#     @classmethod
+#     def setup_class(cls):
+#         cls.qbt = None
+#         cls.qbt_type = FluxQubit
+#         cls.file_str = 'fluxqubit'
+#         cls.op1_str = 'n_1_operator'
+#         cls.op2_str = 'n_2_operator'
+#         cls.param_name = 'flux'
+#         cls.param_list = np.linspace(0.45, 0.55, 50)
