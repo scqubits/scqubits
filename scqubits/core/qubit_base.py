smarter wavefunction scaling & fix issue #85

scqubits/core/qubit_base.py
@@ -30,7 +30,7 @@ from scqubits.core.storage import SpectrumData, DataStore
 from scqubits.settings import IN_IPYTHON
 from scqubits.utils.cpu_switch import get_map_method
 from scqubits.utils.misc import InfoBar, drop_private_keys, process_which
-from scqubits.utils.plot_defaults import set_scaling
+from scqubits.utils.plot_defaults import set_wavefunction_scaling
 from scqubits.utils.spectrum_utils import (get_matrixelement_table, order_eigensystem, recast_esys_mapdata,
                                            standardize_sign)
 
@@ -471,8 +471,8 @@ class QubitBaseClass1d(QubitBaseClass):
     for plotting spectra, matrix elements, and writing data to files.
     """
     # see PEP 526 https://www.python.org/dev/peps/pep-0526/#class-and-instance-variable-annotations
-    _evec_dtype = np.float_
     _default_grid: Grid1d
+    _evec_dtype = np.float_
 
     @abstractmethod
     def potential(self, phi):
@@ -510,31 +510,33 @@ class QubitBaseClass1d(QubitBaseClass):
         -------
         Figure, Axes
         """
-        fig_ax = kwargs.get('fig_ax') or plt.subplots()
-        kwargs['fig_ax'] = fig_ax
+        wavefunc_indices = process_which(which, self.truncated_dim)
 
-        index_list = process_which(which, self.truncated_dim)
         if esys is None:
-            evals_count = max(index_list) + 2
-            esys = self.eigensys(evals_count)
-        evals, _ = esys
+            evals_count = max(wavefunc_indices) + 1
+            evals = self.eigenvals(evals_count=evals_count)
+        else:
+            evals, _ = esys
+
+        energies = evals[list(wavefunc_indices)]
 
         phi_grid = phi_grid or self._default_grid
         potential_vals = self.potential(phi_grid.make_linspace())
 
-        evals_count = len(index_list)
-        if evals_count == 1:
-            scale = set_scaling(self, scaling, potential_vals)
-        else:
-            scale = 0.75 * (evals[-1] - evals[0]) / evals_count
-
         amplitude_modifier = constants.MODE_FUNC_DICT[mode]
-        kwargs = {**self.wavefunction1d_defaults(mode, evals, wavefunc_count=len(index_list)), **kwargs}
-        # in merging the dictionaries in the previous line: if any duplicates, later ones survive
-        for wavefunc_index in index_list:
+        wavefunctions = []
+        for wavefunc_index in wavefunc_indices:
             phi_wavefunc = self.wavefunction(esys, which=wavefunc_index, phi_grid=phi_grid)
             phi_wavefunc.amplitudes = standardize_sign(phi_wavefunc.amplitudes)
             phi_wavefunc.amplitudes = amplitude_modifier(phi_wavefunc.amplitudes)
-            plot.wavefunction1d(phi_wavefunc, potential_vals=potential_vals, offset=phi_wavefunc.energy,
-                                scaling=scale, **kwargs)
+            wavefunctions.append(phi_wavefunc)
+
+        scale = scaling or set_wavefunction_scaling(wavefunctions, potential_vals)
+
+        fig_ax = kwargs.get('fig_ax') or plt.subplots()
+        kwargs['fig_ax'] = fig_ax
+        kwargs = {**self.wavefunction1d_defaults(mode, evals, wavefunc_count=len(wavefunc_indices)), **kwargs}
+        # in merging the dictionaries in the previous line: if any duplicates, later ones survive
+
+        plot.wavefunction1d(wavefunctions, potential_vals=potential_vals, offset=energies, scaling=scale, **kwargs)
         return fig_ax

scqubits/utils/plot_defaults.py
@@ -12,7 +12,6 @@
 
 import numpy as np
 
-import scqubits
 import scqubits.core.constants as constants
 import scqubits.core.units as units
 
@@ -37,27 +36,69 @@ def recast_name(raw_name):
     return raw_name
 
 
-def set_scaling(qubit, scaling, potential_vals=None):
+def set_wavefunction_scaling(wavefunctions, potential_vals):
     """
     Sets the scaling parameter for 1d wavefunctions
 
     Parameters
     ----------
-    qubit: QuantumSystem
-    scaling: float
+    wavefunctions: list of WaveFunction
+        list of all WaveFunction objects to be included in plot and scaling
     potential_vals: ndarray
 
     Returns
     -------
     float
-        scaling factor
+      scaling factor
     """
-    if scaling is None:
-        if isinstance(qubit, scqubits.Transmon):
-            scaling = 0.2 * qubit.EJ
-        elif isinstance(qubit, scqubits.Fluxonium):
-            scaling = 0.125 * (np.max(potential_vals) - np.min(potential_vals))
-    return scaling
+    Y_RANGE_THRESHOLD_FRACTION = 1 / 20  # Do not attempt to scale down amplitudes to very small energy spacings
+                                         # i.e. if energy spacing is smaller than y_range * Y_RANGE_THRESHOLD_FRACTION
+    FILLING_FRACTION = 1.0               # If energy spacing is used for scaling, fill no more than this
+                                         #  fraction of the spacing.
+    MAX_AMPLITUDE_FRACTION = 1 / 7       # Largest allowed wavefunction  amplitude range as fraction of y_range.
+    PRECISION_THRESHOLD = 1E-8           # Amplitude threshold for applying any scaling at all. Note that the
+                                         #  imaginary part of a wavefunction may be nominally 0; do not scale up that.
+
+    wavefunc_count = len(wavefunctions)
+    energies = [wavefunc.energy for wavefunc in wavefunctions]
+
+    y_min = np.min(potential_vals)
+    y_max = max([np.max(potential_vals), np.max(energies)])
+    y_range = y_max - y_min
+
+    amplitudes = np.asarray([wavefunc.amplitudes for wavefunc in wavefunctions])
+
+    def amplitude_mins():
+        return np.apply_along_axis(func1d=np.min, axis=1, arr=amplitudes)
+
+    def amplitude_maxs():
+        return np.apply_along_axis(func1d=np.max, axis=1, arr=amplitudes)
+
+    def max_amplitude_range():
+        return np.max(amplitude_maxs() - amplitude_mins())
+
+    if max_amplitude_range() < PRECISION_THRESHOLD:  # amplitude likely just zero (e.g., mode='imag'); do not scale up
+        return 1
+    else:
+        scale_factor = y_range * MAX_AMPLITUDE_FRACTION / max_amplitude_range()  # set amplitudes to largest acceptable
+        amplitudes *= scale_factor
+
+        if wavefunc_count == 1:
+            return scale_factor
+
+        amplitude_fillings = np.pad(np.abs(amplitude_mins()), [0, 1]) + np.pad(np.abs(amplitude_maxs()), [1, 0])
+        amplitude_fillings = amplitude_fillings[1:-1]
+
+        energy_spacings = np.pad(energies, [0, 1]) - np.pad(energies, [1, 0])
+        energy_spacings = energy_spacings[1:-1]
+
+        for energy_gap, amplitude_filling in zip(energy_spacings, amplitude_fillings):
+            if energy_gap > y_range * Y_RANGE_THRESHOLD_FRACTION:
+                if amplitude_filling > energy_gap * FILLING_FRACTION:
+                    scale_factor *= energy_gap * FILLING_FRACTION / amplitude_filling
+                    amplitudes *= energy_gap * FILLING_FRACTION / amplitude_filling
+                    amplitude_fillings *= energy_gap * FILLING_FRACTION / amplitude_filling
+        return scale_factor
 
 
 def wavefunction1d_discrete(mode=None):

scqubits/utils/plotting.py
@@ -24,6 +24,7 @@ import scqubits.core.constants as constants
 import scqubits.utils.misc as utils
 import scqubits.utils.plot_defaults as defaults
 import scqubits.settings as settings
+import scqubits.core.storage as storage
 
 try:
     from labellines import labelLines
@@ -140,17 +141,17 @@ def _process_special_option(figure, axes, key, value):
         axes.grid(**value) if isinstance(value, dict) else axes.grid(value)
 
 
-def wavefunction1d(wavefunc, potential_vals=None, offset=0, scaling=1, **kwargs):
+def wavefunction1d(wavefuncs, potential_vals=None, offset=0, scaling=1, **kwargs):
     """
     Plots the amplitude of a single real-valued 1d wave function, along with the potential energy if provided.
 
     Parameters
     ----------
-    wavefunc: WaveFunction object
+    wavefuncs: WaveFunction object or list of WaveFunction objects
         basis and amplitude data of wave function to be plotted
     potential_vals: array of float
         potential energies, array length must match basis array of `wavefunc`
-    offset: float
+    offset: float or list of float
         y-offset for the wave function (e.g., shift by eigenenergy)
     scaling: float, optional
         scaling factor for wave function amplitudes
@@ -164,15 +165,20 @@ def wavefunction1d(wavefunc, potential_vals=None, offset=0, scaling=1, **kwargs)
     """
     fig, axes = kwargs.get('fig_ax') or plt.subplots()
 
-    x_vals = wavefunc.basis_labels
-    y_vals = offset + scaling * wavefunc.amplitudes
-    offset_vals = [offset] * len(x_vals)
+    offset_list = [offset] if not isinstance(offset, (list, np.ndarray)) else offset
+    wavefunc_list = [wavefuncs] if not isinstance(wavefuncs, list) else wavefuncs
+
+    for wavefunction, energy_offset in zip(wavefunc_list, offset_list):
+        x_vals = wavefunction.basis_labels
+        y_vals = energy_offset + scaling * wavefunction.amplitudes
+        offset_vals = [energy_offset] * len(x_vals)
+
+        axes.plot(x_vals, y_vals, **_extract_kwargs_options(kwargs, 'plot'))
+        axes.fill_between(x_vals, y_vals, offset_vals, where=(y_vals != offset_vals), interpolate=True)\
 
     if potential_vals is not None:
         axes.plot(x_vals, potential_vals, color='gray', **_extract_kwargs_options(kwargs, 'plot'))
 
-    axes.plot(x_vals, y_vals, **_extract_kwargs_options(kwargs, 'plot'))
-    axes.fill_between(x_vals, y_vals, offset_vals, where=(y_vals != offset_vals), interpolate=True)
     _process_options(fig, axes, **kwargs)
     return fig, axes
 