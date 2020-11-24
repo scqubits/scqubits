dcpd: fix the flux grouping for quasiparticle calculation

scqubits/core/dcpd.py
@@ -484,13 +484,13 @@ class Dcpd(base.QubitBaseClass, serializers.Serializable):
         theta_osc_mat = self._kron3(self._identity_phi(), op.number_sparse(self.dim_theta(), self.theta_plasma()),
                                     self._identity_varphi()) + 2 * self._dis_ec() * self.n_theta_operator() ** 2
 
-        phi_inductive_mat = 0.25 * self._dis_el() * self.phi_operator() ** 2
+        phi_inductive_mat = 0.25 * self._dis_el() * (
+                    self.phi_operator() - 2 * np.pi * self.flux * self.total_identity()) ** 2
         varphi_inductive_mat = 0.5 * self.ELA * self.varphi_operator() ** 2
         add_inductive_mat = self.ELA * (
                 self.varphi_operator() * self.theta_operator() + 2 * np.pi * (self.flux / 2.0 + self.fluxa) * (
                 self.theta_operator() + self.varphi_operator()))
-        phi_flux_term = self._cos_phi_div_operator(2.0) * np.cos(np.pi * self.flux) - self._sin_phi_div_operator(
-            2.0) * np.sin(np.pi * self.flux)
+        phi_flux_term = self._cos_phi_div_operator(2.0)
         junction_mat = - 2 * self.EJ * self._kron3(phi_flux_term, self._identity_theta(),
                                                    self._cos_varphi_div_operator(
                                                        1.0)) + 2 * self.EJ * self.total_identity()
@@ -949,11 +949,11 @@ class Dcpd(base.QubitBaseClass, serializers.Serializable):
 
         if table is True:
             print(' T2_current =', t2_current, ' ms', '\n T2_flux =', t2_flux,
-                         ' ms', '\n T2_flux_a =', t2_fluxa,
-                         ' ms', '\n T1_cap =',
-                         t1_cap, ' ms', '\n T1_ind =', t1_ind, ' ms', '\n T1_qp =', t1_qp, ' ms', '\n T1 =', t1_tot,
-                         ' ms', '\n T2 =', t2_tot,
-                         ' ms')
+                  ' ms', '\n T2_flux_a =', t2_fluxa,
+                  ' ms', '\n T1_cap =',
+                  t1_cap, ' ms', '\n T1_ind =', t1_ind, ' ms', '\n T1_qp =', t1_qp, ' ms', '\n T1 =', t1_tot,
+                  ' ms', '\n T2 =', t2_tot,
+                  ' ms')
 
         return np.array([t2_current, t2_flux, t2_fluxa, t1_cap, t1_ind, t1_qp, t1_tot, t2_tot])
 

scqubits/core/protomon.py
@@ -25,7 +25,7 @@ import scqubits.utils.spectrum_utils as matele_utils
 
 
 # — Inductively-shunted Rhombus circuit ————————————————————————
-class Protomon_update(base.QubitBaseClass, serializers.Serializable):
+class Protomon(base.QubitBaseClass, serializers.Serializable):
     r"""inductively-shunted Rhombus qubit, with the harmonic mode in the ground state
 
     Parameters
@@ -479,7 +479,7 @@ class Protomon_update(base.QubitBaseClass, serializers.Serializable):
         """
         phase drop on additional inductor, used in inductive loss calculation
         """
-        return 0.
+        return self.phi_operator() - self.phi_operator()
 
     def q_ind(self, energy):
         """
@@ -602,23 +602,22 @@ class Protomon_update(base.QubitBaseClass, serializers.Serializable):
                                                       evals_count=cutoff).matrixelem_table[
                     0, init_state, :]
         matelem_2 = np.delete(matelem_2, init_state)
-        # matelem_a = self.get_matelements_vs_paramvals('phase_ind_a_operator', 'ph', [0],
-        #                                               evals_count=cutoff).matrixelem_table[
-        #             0, init_state, :]
-        # matelem_a = np.delete(matelem_a, init_state)
+        matelem_a = self.get_matelements_vs_paramvals('phase_ind_a_operator', 'ph', [0],
+                                                      evals_count=cutoff).matrixelem_table[
+                    0, init_state, :]
+        matelem_a = np.delete(matelem_a, init_state)
 
         s_ii_1 = 2 * np.pi * 2 * self.EL / self.q_ind(np.abs(energy_diff)) * self.thermal_factor(
             energy_diff)
         s_ii_2 = 2 * np.pi * 2 * self.EL / self.q_ind(np.abs(energy_diff)) * self.thermal_factor(
             energy_diff)
-        # s_ii_a = 2 * np.pi * 2 * self.ELA / self.q_ind(np.abs(energy_diff)) * self.thermal_factor(energy_diff)
+        s_ii_a = 2 * np.pi * 2 * self.ELA / self.q_ind(np.abs(energy_diff)) * self.thermal_factor(energy_diff)
 
         gamma1_ind_1 = np.abs(matelem_1) ** 2 * s_ii_1
         gamma1_ind_2 = np.abs(matelem_2) ** 2 * s_ii_2
-        # gamma1_ind_a = np.abs(matelem_a) ** 2 * s_ii_a
+        gamma1_ind_a = np.abs(matelem_a) ** 2 * s_ii_a
 
-        gamma1_ind_tot = np.sum(gamma1_ind_1) + np.sum(gamma1_ind_2) \
-            # + np.sum(gamma1_ind_a)
+        gamma1_ind_tot = np.sum(gamma1_ind_1) + np.sum(gamma1_ind_2) + np.sum(gamma1_ind_a)
         return 1 / (gamma1_ind_tot) * 1e-6
 
     def get_t1_qp_loss(self, init_state):
@@ -650,7 +649,7 @@ class Protomon_update(base.QubitBaseClass, serializers.Serializable):
 
     def get_t2_flux_c_noise(self, init_state):
         """
-        T2 flux noise
+        common flux noise
         """
         delta = 1e-6
         pts = 11
@@ -666,7 +665,7 @@ class Protomon_update(base.QubitBaseClass, serializers.Serializable):
 
     def get_t2_flux_d_noise(self, init_state):
         """
-        T2 flux noise
+        differential flux noise
         """
         delta = 1e-6
         pts = 11
@@ -775,6 +774,6 @@ class Protomon_update(base.QubitBaseClass, serializers.Serializable):
                                                               states[:, j]) * beta_phi + matele_utils.matrix_element(
                             states[:, i], self.n_theta_operator(), states[:, j]) * beta_theta
                         ds_table[i] += np.abs(ds_temp) ** 2 * (1 / (eigsys[0][i] - eigsys[0][j] - w_readout) - 1 / (
-                                    eigsys[0][j] - eigsys[0][i] - w_readout))
+                                eigsys[0][j] - eigsys[0][i] - w_readout))
 
         return ds_table

scqubits/utils/plotting.py
@@ -265,7 +265,7 @@ def contours(x_vals, y_vals, func, contour_vals=None, show_colorbar=True, **kwar
     x_grid, y_grid = np.meshgrid(x_vals, y_vals)
     z_array = func(x_grid, y_grid)
 
-    im = axes.contourf(x_grid, y_grid, z_array, levels=contour_vals, cmap=plt.cm.viridis, origin="lower",
+    im = axes.contourf(x_grid, y_grid, z_array, levels=contour_vals, cmap=plt.cm.bone, origin="lower",
                        **_extract_kwargs_options(kwargs, 'contourf'))
 
     if show_colorbar: