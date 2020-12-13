7eb933aa62cd0093f8fe8991dda1aaed751e81d5
dkweiss31 <dkweiss@u.northwestern.edu>
2020-12-13 10:52:12 -0600

    Testing failures: adding more vchos tests for current mirror to spotlight issue

--- scqubits/tests/vchostest.py
+++ scqubits/tests/vchostest.py
@@ -6,12 +6,70 @@ from scqubits.core.storage import SpectrumData
 
 
 class VCHOSTestFunctions(StandardTests):
+    def test_gamma_matrix(self, io_type):
+        testname = self.file_str + '_1.' + io_type
+        specdata = SpectrumData.create_from_file(DATADIR + testname)
+        self.qbt = self.qbt_type(**specdata.system_params)
+        gamma_matrix = self.qbt.build_gamma_matrix()
+        reference_gamma_matrix = specdata.gamma_matrix
+        assert np.allclose(reference_gamma_matrix, gamma_matrix)
+
+    def test_eigensystem_normal_modes(self, io_type):
+        testname = self.file_str + '_1.' + io_type
+        specdata = SpectrumData.create_from_file(DATADIR + testname)
+        self.qbt = self.qbt_type(**specdata.system_params)
+        omega_squared, normal_mode_eigenvectors = self.qbt.eigensystem_normal_modes()
+        ref_omega_squared, ref_normal_mode_eigenvectors = specdata.eigensystem_normal_modes
+        assert np.allclose(ref_omega_squared, omega_squared)
+        assert np.allclose(np.abs(ref_normal_mode_eigenvectors), np.abs(normal_mode_eigenvectors))
+
+    def test_Xi_matrix(self, io_type):
+        testname = self.file_str + '_1.' + io_type
+        specdata = SpectrumData.create_from_file(DATADIR + testname)
+        self.qbt = self.qbt_type(**specdata.system_params)
+        Xi_matrix = self.qbt.Xi_matrix()
+        reference_Xi_matrix = specdata.Xi_matrix
+        assert np.allclose(reference_Xi_matrix, Xi_matrix)
+
+    def test_relevant_periodic_continuation_vectors(self, io_type):
+        testname = self.file_str + '_1.' + io_type
+        specdata = SpectrumData.create_from_file(DATADIR + testname)
+        self.qbt = self.qbt_type(**specdata.system_params)
+        self.qbt.find_relevant_periodic_continuation_vectors()
+        reference_nearest_neighbors = specdata.nearest_neighbors
+        for key in reference_nearest_neighbors:
+            assert np.allclose(reference_nearest_neighbors[key], self.qbt.nearest_neighbors[key])
+
+    def test_kinetic_matrix(self, io_type):
+        testname = self.file_str + '_1.' + io_type
+        specdata = SpectrumData.create_from_file(DATADIR + testname)
+        self.qbt = self.qbt_type(**specdata.system_params)
+        kinetic_matrix = self.qbt.kinetic_matrix()
+        reference_kinetic_matrix = specdata.kinetic_matrix
+        assert np.allclose(np.abs(reference_kinetic_matrix), np.abs(kinetic_matrix))
+
+    def test_potential_matrix(self, io_type):
+        testname = self.file_str + '_1.' + io_type
+        specdata = SpectrumData.create_from_file(DATADIR + testname)
+        self.qbt = self.qbt_type(**specdata.system_params)
+        potential_matrix = self.qbt.potential_matrix()
+        reference_potential_matrix = specdata.potential_matrix
+        assert np.allclose(np.abs(reference_potential_matrix), np.abs(potential_matrix))
+
+    def test_inner_product_matrix(self, io_type):
+        testname = self.file_str + '_1.' + io_type
+        specdata = SpectrumData.create_from_file(DATADIR + testname)
+        self.qbt = self.qbt_type(**specdata.system_params)
+        inner_product_matrix = self.qbt.inner_product_matrix()
+        reference_inner_product_matrix = specdata.inner_product_matrix
+        assert np.allclose(np.abs(reference_inner_product_matrix), np.abs(inner_product_matrix))
+
     def test_transfer_matrix_comparison(self, io_type):
         testname = self.file_str + '_1.' + io_type
         specdata = SpectrumData.create_from_file(DATADIR + testname)
         self.qbt = self.qbt_type(**specdata.system_params)
         transfer_matrix = self.qbt.transfer_matrix()
-        reference_transfer_matrix = specdata.transfer
+        reference_transfer_matrix = specdata.transfer_matrix
         assert np.allclose(np.abs(reference_transfer_matrix), np.abs(transfer_matrix))
 
     def test_sorted_minima(self, io_type):
