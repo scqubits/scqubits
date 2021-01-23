e1ab46e44f6a9ab6b60eb19178cbd1df08cb3d73
jkochNU <dr.jens.koch@gmail.com>
2021-01-23 16:04:03 -0600

    add `GenericQubit` (pure two-level system)

--- scqubits/__init__.py
+++ scqubits/__init__.py
@@ -24,8 +24,9 @@ from scqubits.core.discretization import Grid1d
 from scqubits.core.explorer import Explorer
 from scqubits.core.flux_qubit import FluxQubit
 from scqubits.core.fluxonium import Fluxonium
+from scqubits.core.generic_qubit import GenericQubit
 from scqubits.core.harmonic_osc import Oscillator
-from scqubits.core.hilbert_space import HilbertSpace, InteractionTerm, InteractionTermStr
+from scqubits.core.hilbert_space import HilbertSpace, InteractionTerm
 from scqubits.core.noise import calc_therm_ratio
 from scqubits.core.param_sweep import ParameterSweep, StoredSweep
 from scqubits.core.storage import SpectrumData, DataStore
