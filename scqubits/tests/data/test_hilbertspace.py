82c8830651fece41a8b3ed81da6ad7c2ed4fb9c8
Elam <99.elam@gmail.com>
2021-02-09 13:39:05 -0600

    List of desired tests added

--- scqubits/tests/data/test_hilbertspace.py
+++
@@ -1,3 +0,0 @@
-# TODO: move to tests
-# TODO: make list of things that are new in hilbertspace and then think of how to
-#  test them
---
+++ scqubits/tests/test_hilbertspace.py
@@ -0,0 +1,19 @@
+
+
+"""
+List of things that could be tested:
+
+The Qtip dict works properly (test sin() or dag() in InteractionTermStr)
+InteractionTermStr and InteractionTerm can handle more than 2 operators being passed in
+Test that you can get correct hamiltonians from ITStr and IT
+The Identity wrapping is working properly on ITStr and IT
+HilbertSpace can handle types of Qobj, ITStr, IT, and ITLegacy all at the same time
+Add hc works for IT and ITStr
+HilbertSpace wont accept additions to the interaction list that are of the wrong type
+
+
+
+
+
+"""
+
