c0318a950f37bdb2e5fe98782a646a363c26a8c4
jkochNU <dr.jens.koch@gmail.com>
2021-07-06 07:29:56 -0500

    refactor and reduce code complexity

--- scqubits/utils/plotting.py
+++ scqubits/utils/plotting.py
@@ -9,15 +9,9 @@
 #    LICENSE file in the root directory of this source tree.
 ############################################################################
 
-import functools
-import operator
-import os
-
 from typing import (
     TYPE_CHECKING,
-    Any,
     Callable,
-    Dict,
     Iterable,
     List,
     Optional,
@@ -32,123 +26,28 @@ import numpy as np
 from matplotlib.axes import Axes
 from matplotlib.figure import Figure
 from mpl_toolkits.axes_grid1 import make_axes_locatable
-from numpy import ndarray
 
 import scqubits.core.constants as constants
-import scqubits.settings as settings
 import scqubits.utils.misc as utils
 import scqubits.utils.plot_defaults as defaults
+from scqubits.utils.plot_utils import (
+    _extract_kwargs_options,
+    _process_options,
+    add_numbers_to_axes,
+    color_normalize,
+    plot_potential_to_axes,
+    plot_wavefunction_to_axes,
+    scale_wavefunctions,
+)
 
 if TYPE_CHECKING:
     from scqubits.core.storage import SpectrumData, WaveFunction, WaveFunctionOnGrid
 
 try:
     from labellines import labelLines
-
     _LABELLINES_ENABLED = True
 except ImportError:
     _LABELLINES_ENABLED = False
-
-
-# A dictionary of plotting options that are directly passed to specific matplotlib's
-# plot commands.
-_direct_plot_options = {
-    "plot": ("alpha", "color", "linestyle", "linewidth", "marker", "markersize"),
-    "imshow": ("interpolation",),
-    "contourf": tuple(),  # empty for now
-}
-
-
-def _extract_kwargs_options(
-    kwargs: Dict[str, Any], plot_type: str, direct_plot_options: Dict[str, Any] = None
-) -> Dict[str, Any]:
-    """
-    Select options from kwargs for a given plot_type and return them in a dictionary.
-
-    Parameters
-    ----------
-    kwargs:
-        dictionary with options that can be passed to different plotting commands
-    plot_type:
-        a type of plot for which the options should be selected
-    direct_plot_options:
-        a lookup dictionary with supported options for a given plot_type
-
-    Returns
-    ----------
-        dictionary with key/value pairs corresponding to selected options from kwargs
-
-    """
-    direct_plot_options = direct_plot_options or _direct_plot_options
-    if plot_type not in direct_plot_options:
-        return {}
-
-    selected_options = {}
-
-    for key in kwargs:
-        if key in direct_plot_options[plot_type]:
-            selected_options[key] = kwargs[key]
-    return selected_options
-
-
-def _process_options(
-    figure: Figure, axes: Axes, opts: Dict[str, Any] = None, **kwargs
-) -> None:
-    """
-    Processes plotting options.
-
-    Parameters
-    ----------
-    figure:
-    axes:
-    opts:
-        keyword dictionary with custom options
-    **kwargs:
-        standard plotting option (see separate documentation)
-    """
-    opts = opts or {}
-
-    # Only process items in kwargs that would not have been
-    # processed through _extract_kwargs_options()
-    filtered_kwargs = {
-        key: value
-        for key, value in kwargs.items()
-        if key not in functools.reduce(operator.concat, _direct_plot_options.values())
-    }  # type: ignore
-
-    option_dict = {**opts, **filtered_kwargs}
-
-    for key, value in option_dict.items():
-        if key in defaults.SPECIAL_PLOT_OPTIONS:
-            _process_special_option(figure, axes, key, value)
-        else:
-            set_method = getattr(axes, "set_" + key)
-            set_method(value)
-
-    filename = kwargs.get("filename")
-    if filename:
-        figure.savefig(os.path.splitext(filename)[0] + ".pdf")
-
-    if settings.DESPINE and not axes.name == "3d":
-        despine_axes(axes)
-
-
-def _process_special_option(figure: Figure, axes: Axes, key: str, value: Any) -> None:
-    """Processes a single 'special' option, i.e., one internal to scqubits and not to be
-    handed further down to matplotlib.
-    """
-    if key == "ymax":
-        ymax = value
-        ymin, _ = axes.get_ylim()
-        ymin = ymin - (ymax - ymin) * 0.05
-        axes.set_ylim(ymin, ymax)
-    elif key == "figsize":
-        figure.set_size_inches(value)
-    elif key == "grid":
-        if isinstance(value, dict):
-            axes.grid(**value)
-        else:
-            axes.grid(value)
 
 
 def wavefunction1d(
@@ -186,9 +85,7 @@ def wavefunction1d(
     wavefunc_list = scale_wavefunctions(wavefunc_list, potential_vals, scaling)
 
     for wavefunction, energy_offset in zip(wavefunc_list, offset_list):
-        plot_wavefunction_to_axes(
-            axes, wavefunction, energy_offset, **kwargs
-        )
+        plot_wavefunction_to_axes(axes, wavefunction, energy_offset, **kwargs)
 
     if potential_vals is not None:
         x_vals = wavefunc_list[0].basis_labels
@@ -403,34 +300,22 @@ def matrix_skyscraper(
     modefunction = constants.MODE_FUNC_DICT[mode]
     zheight = modefunction(matrix).flatten()  # height of bars from matrix elements
 
-    min_zheight, max_zheight = min(zheight), max(zheight)
-
-    if mode == "abs" or mode == "abs_sqr":
-        nrm = mpl.colors.Normalize(
-            0, max_zheight
-        )  # normalize colors between 0 and max. data
-    else:
-        nrm = mpl.colors.Normalize(
-            min_zheight, max_zheight
-        )  # normalize colors between min. and max. of data
-
+    min_zheight, max_zheight, nrm = color_normalize(zheight, mode)
     colors = plt.cm.viridis(nrm(zheight))  # list of colors for each bar
 
     # skyscraper plot
     axes.view_init(azim=210, elev=23)
     axes.bar3d(xgrid, ygrid, zbottom, dx, dy, zheight, color=colors)
 
-    if mode == "abs" or mode == "abs_sqr":
-        min_z, max_z = 0, max_zheight
-    else:  # mode is "real" or "imag"
-        min_z = 0 if min_zheight > 0 else min_zheight
-        max_z = 0 if max_zheight < 0 else max_zheight
+    if mode in ["real", "imag"]:
+        min_zheight = 0 if min_zheight > 0 else min_zheight
+        max_zheight = 0 if max_zheight < 0 else max_zheight
 
-    if min_z == max_z:
+    if min_zheight == max_zheight:
         # pad with small values so we don't get warnings
-        max_z += 0.0000001
+        max_zheight += 0.0000001
 
-    axes.set_zlim3d([min_z, max_z])
+    axes.set_zlim3d([min_zheight, max_zheight])
 
     for axis, locs in [
         (axes.xaxis, np.arange(x_count)),
@@ -472,14 +357,7 @@ def matrix2d(
     modefunction = constants.MODE_FUNC_DICT[mode]
     zheight = modefunction(matrix).flatten()  # height of bars from matrix elements
 
-    if mode == "abs" or mode == "abs_sqr":
-        nrm = mpl.colors.Normalize(
-            0, max(zheight)
-        )  # normalize colors between 0 and max. data
-    else:
-        nrm = mpl.colors.Normalize(
-            min(zheight), max(zheight)
-        )  # normalize colors between min. and max. of data
+    min_zheight, max_zheight, nrm = color_normalize(zheight, mode)
 
     axes.matshow(modefunction(matrix), cmap=plt.cm.viridis, interpolation=None)
     cax, _ = mpl.colorbar.make_axes(
@@ -488,18 +366,8 @@ def matrix2d(
     mpl.colorbar.ColorbarBase(cax, cmap=plt.cm.viridis, norm=nrm)
 
     if show_numbers:
-        for y_index in range(matrix.shape[0]):
-            for x_index in range(matrix.shape[1]):
-                axes.text(
-                    x_index,
-                    y_index,
-                    "{:.03f}".format(modefunction(matrix[y_index, x_index])),
-                    va="center",
-                    ha="center",
-                    fontsize=8,
-                    rotation=45,
-                    color="white",
-                )
+        add_numbers_to_axes(axes, matrix, modefunction)
+
     # shift the grid
     for axis, locs in [
         (axes.xaxis, np.arange(matrix.shape[1])),
@@ -664,62 +532,3 @@ def matelem_vs_paramvals(
         axes.legend(loc="center left", bbox_to_anchor=(1, 0.5))
     _process_options(fig, axes, opts=defaults.matelem_vs_paramvals(specdata), **kwargs)
     return fig, axes
-
-
-def despine_axes(axes: Axes) -> None:
-    # Hide the right and top spines
-    axes.spines["right"].set_visible(False)
-    axes.spines["top"].set_visible(False)
-
-    # Only show ticks on the left and bottom spines
-    axes.yaxis.set_ticks_position("left")
-    axes.xaxis.set_ticks_position("bottom")
-
-
-def scale_wavefunctions(
-    wavefunc_list: List["WaveFunction"], potential_vals: np.ndarray, scaling
-) -> List["WaveFunction"]:
-    for wavefunc in wavefunc_list:
-        wavefunc.rescale_to_potential(potential_vals)
-    adaptive_scalefactor = scaling or defaults.set_wavefunction_scaling(
-        wavefunc_list, potential_vals
-    )
-    for wavefunc in wavefunc_list:
-        wavefunc.rescale(adaptive_scalefactor)
-    return wavefunc_list
-
-
-def plot_wavefunction_to_axes(
-    axes: Axes,
-    wavefunction: "WaveFunction",
-    energy_offset: float,
-    **kwargs
-) -> None:
-    x_vals = wavefunction.basis_labels
-    y_vals = energy_offset + wavefunction.amplitudes
-    offset_vals = [energy_offset] * len(x_vals)
-
-    axes.plot(x_vals, y_vals, **_extract_kwargs_options(kwargs, "plot"))
-    axes.fill_between(
-        x_vals, y_vals, offset_vals, where=(y_vals != offset_vals), interpolate=True
-    )
-
-
-def plot_potential_to_axes(
-    axes: Axes,
-    x_vals: ndarray,
-    potential_vals: Union[ndarray, List[float]],
-    offset_list: Union[ndarray, List[float]],
-    **kwargs
-) -> None:
-    y_min = np.min(potential_vals)
-    y_max = np.max(offset_list)
-    y_range = y_max - y_min
-
-    y_max += 0.3 * y_range
-    y_min = np.min(potential_vals) - 0.1 * y_range
-    axes.set_ylim([y_min, y_max])
-
-    axes.plot(
-        x_vals, potential_vals, color="gray", **_extract_kwargs_options(kwargs, "plot")
-    )
