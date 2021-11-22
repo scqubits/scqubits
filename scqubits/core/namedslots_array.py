312752de08da2c004b6d644d7c303a295afd61c1
jkochNU <dr.jens.koch@gmail.com>
2021-11-22 09:37:49 -0600

    lint

--- scqubits/core/namedslots_array.py
+++ scqubits/core/namedslots_array.py
@@ -487,8 +487,8 @@ class NamedSlotsNdarray(np.ndarray, Serializable):
     def __new__(
         cls, input_array: np.ndarray, values_by_name: Dict[str, ndarray]
     ) -> "NamedSlotsNdarray":
-        implied_shape = tuple(len(values) for name, values in values_by_name.items())
-        if input_array.shape[0 : len(values_by_name)] != implied_shape:
+        implied_shape = tuple(len(values) for values in values_by_name.values())
+        if input_array.shape[0: len(values_by_name)] != implied_shape:
             raise ValueError(
                 "Given input array {} with shape {} not compatible with "
                 "provided dict calling for shape {}. values_by_name: {}".format(
