e01ad08736363f96b0eb74511522623db743ba42
jkochNU <dr.jens.koch@gmail.com>
2022-01-19 20:18:16 -0600

    docstring correction for `TunableTransmon` (issue #117)

--- scqubits/core/transmon.py
+++ scqubits/core/transmon.py
@@ -374,7 +374,7 @@ class Transmon(base.QubitBaseClass1d, serializers.Serializable, NoisySystem):
 class TunableTransmon(Transmon, serializers.Serializable, NoisySystem):
     r"""Class for the flux-tunable transmon qubit. The Hamiltonian is represented in
     dense form in the number basis, :math:`H_\text{CPB}=4E_\text{C}(\hat{
-    n}-n_g)^2+\frac{\mathcal{E}_\text{J}(\Phi)}{2}(|n\rangle\langle n+1|+\text{
+    n}-n_g)^2-\frac{\mathcal{E}_\text{J}(\Phi)}{2}(|n\rangle\langle n+1|+\text{
     h.c.})`, Here, the effective Josephson energy is flux-tunable: :math:`\mathcal{
     E}_J(\Phi) = E_{J,\text{max}} \sqrt{\cos^2(\pi\Phi/\Phi_0) + d^2 \sin^2(
     \pi\Phi/\Phi_0)}` and :math:`d=(E_{J2}-E_{J1})(E_{J1}+E_{J2})` parametrizes the
