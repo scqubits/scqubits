b34da5501ece302ad82fb44507c433882b703cab
jkochNU <dr.jens.koch@gmail.com>
2022-04-02 15:10:28 -0500

    further Explorer refactoring

--- scqubits/explorer/explorer_panels.py
+++ scqubits/explorer/explorer_panels.py
@@ -170,45 +170,6 @@ def display_bare_wavefunctions(
     )
 
 
-# def display_dressed_spectrum(
-#     sweep: "ParameterSweep",
-#     initial_bare: Tuple[int, ...],
-#     final_bare: Tuple[int, ...],
-#     energy_initial: float,
-#     energy_final: float,
-#     param_slice: "ParameterSlice",
-#     fig_ax: Tuple[Figure, Axes],
-# ) -> None:
-#     energy_difference = energy_final - energy_initial
-#     title = r"{} $\rightarrow$ {}: {:.4f} {}".format(
-#         initial_bare, final_bare, energy_difference, units.get_units()
-#     )
-#     fig, axes = sweep[param_slice.fixed].plot_transitions(title=title, fig_ax=fig_ax)
-#     axes.axvline(param_slice.param_val, color="gray", linestyle=":")
-#     axes.scatter(
-#         [param_slice.param_val] * 2, [energy_initial, energy_final], s=40, c="gray"
-#     )
-
-
-# def display_n_photon_qubit_transitions(
-#     sweep: "ParameterSweep",
-#     photonnumber: int,
-#     subsys: "QubitBaseClass",
-#     initial_bare: Tuple[int, ...],
-#     param_slice: "ParameterSlice",
-#     fig_ax: Tuple[Figure, Axes],
-# ) -> None:
-#     title = r"{}-photon {} transitions".format(photonnumber, subsys.id_str)
-#     fig, axes = sweep[param_slice.fixed].plot_transitions(
-#         subsystems=[subsys],
-#         initial=initial_bare,
-#         photon_number=photonnumber,
-#         title=title,
-#         fig_ax=fig_ax,
-#     )
-#     axes.axvline(param_slice.param_val, color="gray", linestyle=":")
-
-
 def display_transitions(
     sweep: "ParameterSweep",
     photon_number: int,
@@ -231,21 +192,6 @@ def display_transitions(
         fig_ax=fig_ax,
     )
     axes.axvline(param_slice.param_val, color="gray", linestyle=":")
-
-
-# def display_chi_01(
-#     sweep: "ParameterSweep",
-#     subsys1_index: int,
-#     subsys2_index: int,
-#     param_val: float,
-#     fig_ax: Tuple[Figure, Axes],
-# ) -> None:
-#     chi_data = sweep["chi"][subsys1_index, subsys2_index]
-#     title = r"$\chi_{01}$" + " = {:.4f}{}".format(
-#         chi_data[float(param_val), 1], units.get_units()
-#     )
-#     fig, axes = chi_data[:, 1].plot(title=title, fig_ax=fig_ax)
-#     axes.axvline(param_val, color="gray", linestyle=":")
 
 
 def display_cross_kerr(
@@ -329,44 +275,3 @@ def display_self_kerr(
         fig_ax=fig_ax,
     )
     axes.axvline(param_slice.param_val, color="gray", linestyle=":")
-
-
-
-#
-#
-# def display_kerrlike(
-#     sweep: "ParameterSweep",
-#     subsys1_index: int,
-#     subsys2_index: int,
-#     param_val: float,
-#     fig_ax: Tuple[Figure, Axes],
-# ) -> None:
-#     subsys2 = sweep.get_subsys(subsys2_index)
-#
-#     if subsys2 in sweep.osc_subsys_list:
-#         display_chi_01(sweep, subsys1_index, subsys2_index, param_val, fig_ax)
-#     else:
-#         display_kerrs(sweep, subsys1_index, subsys2_index, param_val, fig_ax)
-
-
-# def display_charge_matrixelems(
-#     sweep: "ParameterSweep",
-#     initial_bare: Tuple[int, ...],
-#     subsys_index: int,
-#     param_val: float,
-#     fig_ax: Tuple[Figure, Axes],
-# ) -> None:
-#     subsys = sweep.get_subsys(subsys_index)
-#     bare_qbt_initial = initial_bare[subsys_index]
-#     title = r"charge matrix elements for {}".format(subsys.id_str)
-#     charge_matrixelems = np.abs(
-#         sweep["n_operator qubit " + str(subsys_index)][:, bare_qbt_initial, :]
-#     )
-#     indices = range(charge_matrixelems.shape[1])
-#     fig, axes = charge_matrixelems.plot(
-#         title=title,
-#         ylabel=r"$|\langle i |n| j \rangle|$",
-#         label_list=["{},{}".format(bare_qbt_initial, fin) for fin in indices],
-#         fig_ax=fig_ax,
-#     )
-#     axes.axvline(param_val, color="gray", linestyle=":")
