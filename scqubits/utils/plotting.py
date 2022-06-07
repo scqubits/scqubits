721d7db4c3d18b6aa136cde408ba96c75293d1c6
Or Golan <golanor@users.noreply.github.com>
2022-06-07 09:18:50 +0300

    changed font size scaling to work for non square figures

--- scqubits/utils/plotting.py
+++ scqubits/utils/plotting.py
@@ -361,10 +361,9 @@ def matrix2d(
 
     if show_numbers:
         fig_width, fig_height = fig.get_size_inches()
-        fig_width, fig_height = fig_width, fig_height
-        number_of_boxes = matrix.shape[0] * matrix.shape[1]
-        min_scaling_size = np.round(fig.dpi * fig_width * fig_height / number_of_boxes) + 1  # font size of many boxes
-        font_size = min(min_scaling_size, 9)
+        box_width_inches = fig_width / matrix.shape[1]  
+        box_height_inches = fig_height / matrix.shape[0]
+        font_size = min(box_width_inches, box_height_inches) * 12
         add_numbers_to_axes(axes, matrix, modefunction, fontsize=font_size)
 
     # shift the grid
