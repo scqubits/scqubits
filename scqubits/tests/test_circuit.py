042c0828ab24289c3950b335b3f4c64ab112fdc5
Sai Pavan Chitta <saipavanchitta1998@gmail.com>
2022-06-23 21:15:43 -0500

    Fixed pytest and updated files for github workflow.

--- scqubits/tests/test_circuit.py
+++ scqubits/tests/test_circuit.py
@@ -57,7 +57,7 @@ def test_eigenvals_harmonic():
 
 
 def test_eigenvals_discretized():
-    ref_eigs = np.array([0.0, 0.03613524, 0.05924472, 0.09538, 4.37899011, 4.41520237])
+    ref_eigs = np.array([0., 0.03559217, 0.05819503, 0.09378266, 4.39921833, 4.43482385])
     DFC = scq.Circuit(
         DATADIR + "circuit_DFC.yaml",
         ext_basis="discretized",
