0b2a97237526a7e79ebf7951e273ab4a9a1b4eee
Danyang Chen <dychen1999@foxmail.com>
2022-06-27 14:19:49 -0500

    Lamb shifts for oscillator modes

--- scqubits/core/param_sweep.py
+++ scqubits/core/param_sweep.py
@@ -1225,15 +1225,29 @@ class ParameterSweep(  # type:ignore
         for subsys_index1, subsys1 in enumerate(self.hilbertspace):
             energy_subsys1_all_l1 = self._energies_1(subsys1)
             bare_energy_subsys1_all_l1 = self["bare_evals"][subsys_index1].toarray()
-            lamb_subsys1_all_l1 = (
-                energy_subsys1_all_l1
-                - energy_0[..., None]
-                - bare_energy_subsys1_all_l1
-                + bare_energy_subsys1_all_l1[..., 0][..., None]
-            )
-            lamb_data[subsys_index1] = NamedSlotsNdarray(
-                lamb_subsys1_all_l1, self._parameters.paramvals_by_name
-            )
+
+            # Lamb shifts for oscillator modes
+            if subsys1 in self.osc_subsys_list:
+                lamb_subsys1_all_l1 = (
+                    energy_subsys1_all_l1[..., 1]
+                    - energy_0
+                    - bare_energy_subsys1_all_l1[..., 1]
+                    + bare_energy_subsys1_all_l1[..., 0]
+                )
+                lamb_data[subsys_index1] = NamedSlotsNdarray(
+                    lamb_subsys1_all_l1, self._parameters.paramvals_by_name
+                )
+            # Lamb shifts for qubit modes
+            else:
+                lamb_subsys1_all_l1 = (
+                    energy_subsys1_all_l1
+                    - energy_0[..., None]
+                    - bare_energy_subsys1_all_l1
+                    + bare_energy_subsys1_all_l1[..., 0][..., None]
+                )
+                lamb_data[subsys_index1] = NamedSlotsNdarray(
+                    lamb_subsys1_all_l1, self._parameters.paramvals_by_name
+                )
 
         # Kerr and ac Stark
         for subsys_index1, subsys1 in enumerate(self.hilbertspace):
