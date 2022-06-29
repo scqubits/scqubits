e10d4a03c45f22ae5ebc8031fc7cb6594d74d73f
jkochNU <dr.jens.koch@gmail.com>
2022-06-29 17:27:55 -0500

    lint

--- scqubits/tests/test_circuit_plot.py
+++ scqubits/tests/test_circuit_plot.py
@@ -1,9 +1,9 @@
-from scqubits.core.circuit_utils import example_circuit
-from scqubits import truncation_template
-import scqubits as scq
+import os
 import numpy as np
 
-import os
+import scqubits as scq
+from scqubits import truncation_template
+from scqubits.core.circuit_utils import example_circuit
 
 TESTDIR, _ = os.path.split(scq.__file__)
 TESTDIR = os.path.join(TESTDIR, "tests", "")
