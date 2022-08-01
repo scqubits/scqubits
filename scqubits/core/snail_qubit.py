b6b221e6cc85db125b6030aac19ee661492b82b4
jkochNU <jens-koch@northwestern.edu>
2022-08-01 13:15:12 -0400

    changes for `.create()` functionality

--- scqubits/core/snail_qubit.py
+++ scqubits/core/snail_qubit.py
@@ -1,48 +1,55 @@
-import numpy as np
-import scipy as sp
-from scipy import sparse
-
 from abc import ABC, abstractmethod
 from typing import Any, Dict, List, Optional, Tuple
-from scqubits.core.qubit_base import QubitBaseClass
+
 import matplotlib.pyplot as plt
+import numpy as np
+import scipy as sp
+
 from numpy import ndarray
+from scipy import sparse
+
+import scqubits.core.qubit_base as base
+import scqubits.io_utils.fileio_serializers as serializers
 
 
-class SnailQubit(QubitBaseClass):
-
-    def __init__(self,
-                 E_J1: float,
-                 E_J2: float,
-                 E_J3: float,
-                 E_J4: float,
-                 E_C1: float,
-                 E_C2: float,
-                 E_C3: float,
-                 E_C4: float,
-                 E_cg1: float,
-                 E_cg2: float,
-                 E_cg3: float,
-                 ng1: float,
-                 ng2: float,
-                 ng3: float,
-                 flux: float,
-                 n_cut: int) -> None:
-
+class SnailQubit(base.QubitBaseClass, serializers.Serializable):
+    def __init__(
+        self,
+        EJ1: float,
+        EJ2: float,
+        EJ3: float,
+        EJ4: float,
+        EC1: float,
+        EC2: float,
+        EC3: float,
+        EC4: float,
+        ECg1: float,
+        ECg2: float,
+        ECg3: float,
+        ng1: float,
+        ng2: float,
+        ng3: float,
+        flux: float,
+        ncut: int,
+        id_str: Optional[str] = None,
+    ) -> None:
+        base.QuantumSystem.__init__(self, id_str=id_str)
         # Constant parameters for each Josephson Junction
-        self.E_J1 = E_J1
-        self.E_J2 = E_J2
-        self.E_J3 = E_J3
-        self.E_J4 = E_J4
+        self.EJ1 = EJ1
+        self.EJ2 = EJ2
+        self.EJ3 = EJ3
+        self.EJ4 = EJ4
         # Capacitance connected to each Josephson Junction
-        self.c1 = 1 / (2 * E_C1)
-        self.c2 = 1 / (2 * E_C2)
-        self.c3 = 1 / (2 * E_C3)
-        self.c4 = 1 / (2 * E_C4)
+        self.EC1 = EC1
+        self.EC2 = EC2
+        self.EC3 = EC3
+        self.EC4 = EC4
+
         # Capacitance connected to ground
-        self.cg1 = 1 / (2 * E_cg1)
-        self.cg2 = 1 / (2 * E_cg2)
-        self.cg3 = 1 / (2 * E_cg3)
+        self.ECg1 = ECg1
+        self.ECg2 = ECg2
+        self.ECg3 = ECg3
+
         # offset charges associated with each Josephson Junction
         self.ng1 = ng1
         self.ng2 = ng2
@@ -50,19 +57,21 @@ class SnailQubit(QubitBaseClass):
         # flux
         self.flux = flux
         # Truncation dimension
-        self.n_cut = n_cut
-        self.dim = 2 * n_cut + 1
+        self.ncut = ncut
+        self.dim = 2 * ncut + 1
+        self._image_filename = None
 
-    def default_params(self) -> Dict[str, Any]:
+    @staticmethod
+    def default_params() -> Dict[str, Any]:
         return {
             "EJ1": 1.0,
             "EJ2": 1.0,
             "EJ3": 1.0,
-            "EJ4":0.7,
-            "ECJ1": 1,
-            "ECJ2": 1,
-            "ECJ3": 1,
-            "ECJ4": 1/0.7,
+            "EJ4": 0.7,
+            "EC1": 1,
+            "EC2": 1,
+            "EC3": 1,
+            "EC4": 1 / 0.7,
             "ECg1": 10,
             "ECg2": 10,
             "ECg3": 10,
@@ -70,72 +79,132 @@ class SnailQubit(QubitBaseClass):
             "ng2": 0.0,
             "ng3": 0.0,
             "flux": 0.41,
-            "ncut": 10
+            "ncut": 10,
         }
 
-    # Construct the Ec matrix, we need this to calculate the kinetic_energy matrix in the Hamiltonian
+    # Construct the Ec matrix, we need this to calculate the kinetic_energy matrix in
+    # the Hamiltonian
     def get_Ec_matrix(self) -> ndarray:
-        capacitance = np.array([[self.c1 + self.c2 + self.cg1, - self.c2, 0],
-                        [- self.c2, self.c2 + self.c3 + self.cg2, - self.c3],
-                        [0, - self.c3, self.c3 + self.c4 + self.cg3]])
-        E_c = np.linalg.inv(capacitance) * (1 / 2)
-        return E_c
+        c1 = 1 / (2 * self.EC1)
+        c2 = 1 / (2 * self.EC2)
+        c3 = 1 / (2 * self.EC3)
+        c4 = 1 / (2 * self.EC4)
 
+        cg1 = 1 / (2 * self.ECg1)
+        cg2 = 1 / (2 * self.ECg2)
+        cg3 = 1 / (2 * self.ECg3)
+        capacitance = np.array(
+            [
+                [c1 + c2 + cg1, -c2, 0],
+                [-c2, c2 + c3 + cg2, -c3],
+                [0, -c3, c3 + c4 + cg3],
+            ]
+        )
+        E_c = 0.5 * np.linalg.inv(capacitance)
+        return E_c
 
     def get_kinetic(self) -> ndarray:
         ec = self.get_Ec_matrix()
         identity = sparse.identity(self.dim, format="csc")
 
-        n1 = np.arange(-self.n_cut, self.n_cut + 1, 1)
+        n1 = np.arange(-self.ncut, self.ncut + 1, 1)
         n1 = sparse.diags(n1).tocsc()
-        n1 = sparse.kron(sparse.kron(n1, identity, format="csc"), identity, format="csc")
+        n1 = sparse.kron(
+            sparse.kron(n1, identity, format="csc"), identity, format="csc"
+        )
 
-        n2 = np.arange(-self.n_cut, self.n_cut + 1, 1)
+        n2 = np.arange(-self.ncut, self.ncut + 1, 1)
         n2 = sparse.diags(n2).tocsc()
-        n2 = sparse.kron(sparse.kron(identity, n2, format="csc"), identity, format="csc")
+        n2 = sparse.kron(
+            sparse.kron(identity, n2, format="csc"), identity, format="csc"
+        )
 
-        n3 = np.arange(-self.n_cut, self.n_cut + 1, 1)
+        n3 = np.arange(-self.ncut, self.ncut + 1, 1)
         n3 = sparse.diags(n3).tocsc()
-        n3 = sparse.kron(sparse.kron(identity, identity, format="csc"), n3, format="csc")
+        n3 = sparse.kron(
+            sparse.kron(identity, identity, format="csc"), n3, format="csc"
+        )
 
         nvec = np.array([n1, n2, n3])
 
         return 4 * nvec.T @ ec @ nvec
 
-
     def potential(self, phi1, phi2, phi3) -> float:
         """Return value of the potential energy at phi1 and phi2 and phi3, disregarding
         constants."""
         return (
-            - self.E_J1 * np.cos(phi1)
-            - self.E_J2 * np.cos(phi2-phi1)
-            - self.E_J3 * np.cos(phi3-phi2)
-            - self.E_J4 * np.cos(2.0 * np.pi * self.flux - phi3)
+            -self.EJ1 * np.cos(phi1)
+            - self.EJ2 * np.cos(phi2 - phi1)
+            - self.EJ3 * np.cos(phi3 - phi2)
+            - self.EJ4 * np.cos(2.0 * np.pi * self.flux - phi3)
         )
 
     def get_potential(self) -> ndarray:
         identity = sparse.identity(self.dim, format="csc")
 
-        ones_on_diagonal = np.ones((1, self.dim-1))  # might cause problem
-        e_positive_phi = sparse.diags(ones_on_diagonal, [ 1]).tocsc()
+        ones_on_diagonal = np.ones((1, self.dim - 1))  # might cause problem
+        e_positive_phi = sparse.diags(ones_on_diagonal, [1]).tocsc()
         e_negative_phi = sparse.diags(ones_on_diagonal, [-1]).tocsc()
 
-        component1 = -(0.5) * self.E_J1 * sparse.kron(sparse.kron(e_positive_phi + e_negative_phi, identity, format="csc"), identity, format="csc")
-        component2 = -(0.5) * self.E_J4 * sparse.kron(sparse.kron(identity, identity, format="csc"),
-                                                        np.exp(-1j * 2 * np.pi * self.flux)* e_positive_phi
-                                                        + np.exp(+1j * 2 * np.pi * self.flux)* e_negative_phi,
-                                                        format="csc")
-        component3 = -(0.5) * self.E_J2 * sparse.kron(sparse.kron(e_positive_phi, e_negative_phi, format="csc"),
-                                                        identity, format="csc")
-        component4 = -(0.5) * self.E_J2 * sparse.kron(sparse.kron(e_negative_phi, e_positive_phi, format="csc"),
-                                                        identity, format="csc")
-        component5 = -(0.5) * self.E_J3 * sparse.kron(sparse.kron(identity, e_positive_phi, format="csc"),
-                                                        e_negative_phi, format="csc")
-        component6 = -(0.5) * self.E_J3 * sparse.kron(sparse.kron(identity, e_negative_phi, format="csc"),
-                                                        e_positive_phi, format="csc")
+        component1 = (
+            -0.5
+            * self.EJ1
+            * sparse.kron(
+                sparse.kron(e_positive_phi + e_negative_phi, identity, format="csc"),
+                identity,
+                format="csc",
+            )
+        )
+        component2 = (
+            -0.5
+            * self.EJ4
+            * sparse.kron(
+                sparse.kron(identity, identity, format="csc"),
+                np.exp(-1j * 2 * np.pi * self.flux) * e_positive_phi
+                + np.exp(+1j * 2 * np.pi * self.flux) * e_negative_phi,
+                format="csc",
+            )
+        )
+        component3 = (
+            -0.5
+            * self.EJ2
+            * sparse.kron(
+                sparse.kron(e_positive_phi, e_negative_phi, format="csc"),
+                identity,
+                format="csc",
+            )
+        )
+        component4 = (
+            -0.5
+            * self.EJ2
+            * sparse.kron(
+                sparse.kron(e_negative_phi, e_positive_phi, format="csc"),
+                identity,
+                format="csc",
+            )
+        )
+        component5 = (
+            -0.5
+            * self.EJ3
+            * sparse.kron(
+                sparse.kron(identity, e_positive_phi, format="csc"),
+                e_negative_phi,
+                format="csc",
+            )
+        )
+        component6 = (
+            -0.5
+            * self.EJ3
+            * sparse.kron(
+                sparse.kron(identity, e_negative_phi, format="csc"),
+                e_positive_phi,
+                format="csc",
+            )
+        )
 
-
-        return  component1 +  component2 + component3 + component4 + component5 + component6
+        return (
+            component1 + component2 + component3 + component4 + component5 + component6
+        )
 
     def hamiltonian(self) -> ndarray:
         """Return the Hamiltonian."""
@@ -147,21 +216,16 @@ class SnailQubit(QubitBaseClass):
 
     def hilbertdim(self) -> int:
         """Return Hilbert space dimension."""
-        return (2 * self.n_cut + 1) ** 3
+        return (2 * self.ncut + 1) ** 3
 
     def _evals_calc(self, evals_count: int) -> ndarray:
         hamiltonian_mat = self.hamiltonian()
         evals = sp.sparse.linalg.eigsh(
-                hamiltonian_mat, which='SM',k = evals_count, return_eigenvectors=False
-            )
+            hamiltonian_mat, which="SM", k=evals_count, return_eigenvectors=False
+        )
         return np.sort(evals)
 
-    def plot_potential(
-        self,
-        phi_grid = None,
-        contour_vals= None,
-        **kwargs
-    ) :
+    def plot_potential(self, phi_grid=None, contour_vals=None, **kwargs):
         """
         Draw contour plot of the potential energy.
 
@@ -174,19 +238,7 @@ class SnailQubit(QubitBaseClass):
         **kwargs:
             plot options
         """
-        x_vals = 0.003 * np.arange(-800,800)
+        x_vals = 0.003 * np.arange(-800, 800)
         if "figsize" not in kwargs:
             kwargs["figsize"] = (5, 5)
-        return plt.plot(
-            x_vals, self.potential(x_vals,x_vals,x_vals)/self.E_J4)
-
-test = SnailQubit(1, 1, 1, 0.7, 1, 1, 1, 1/0.7, 10, 10, 10, 0, 0, 0, 0.41, 10)
-#print("\nmat_mul H:\n------------------------------------------------------\n")
-#print(test.hamiltonian())
-print(test._evals_calc(8))
-#print(test.hilbertdim())
-#plt.title("Nonlinear Asymmetric Potential")
-#plt.xlabel("Flux reduced to 1 DoF")
-#plt.ylabel("Potential Energy (U)")
-#test.plot_potential()
-#plt.show()
+        return plt.plot(x_vals, self.potential(x_vals, x_vals, x_vals) / self.EJ4)
