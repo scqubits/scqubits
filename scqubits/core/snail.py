4cc11fff4d730ac1165507fe38a2f97567568f82
jkochNU <jens-koch@northwestern.edu>
2022-08-07 17:44:14 -0600

    manual merge (docstrings, default values)

--- scqubits/core/snail.py
+++ scqubits/core/snail.py
@@ -1,14 +1,19 @@
-import numpy as np
-from scipy import sparse
 from typing import Any, Dict, Optional, Tuple
-import scqubits.core.qubit_base as base
-from scqubits.core import descriptors
-from scqubits.core.qubit_base import QubitBaseClass
+
+import matplotlib.pyplot as plt
+import numpy as np
+
 from numpy import ndarray
+from scipy import sparse
+
+import scqubits.core.qubit_base as base
+import scqubits.io_utils.fileio_serializers as serializers
 import scqubits.utils.spectrum_utils as utils
 
+from scqubits.core import descriptors
 
-class Snail(QubitBaseClass):
+
+class Snailmon(base.QubitBaseClass, serializers.Serializable):
     r"""SNAIL
 
     | [1] Frattini et al., Appl. Phys Lett. 110, 222603 (2017). https://doi.org/10.1063/1.4984142
@@ -19,11 +24,12 @@ class Snail(QubitBaseClass):
     of a superconducting loop of n large Josephson Junctions and one smaller
     junction. It was designed in order to obtain :math:`\textbf{\phi}^3` nonlinearity
     from a dipole element as opposed to previous attempts (such as the Josephson ring
-    modulator) that relied on a quadrupole element, and it can best be understood as an extension of the flux qubit
-    (first defined in [2]) to n large Josephson junctions instead of 2. The greatest number of large junctions
-    that has been experimentally realized in a SNAIL qubit is 3. Typically, one assumes
-    :math:`E_{J1}=E_{J2}=E_{J3}=E_J` and :math:`E_{J4}=\alpha E_J`. In the case of 3 Josephson Junctions, the
-    Hamiltonian is given by
+    modulator) that relied on a quadrupole element, and it can best be understood as an
+    extension of the flux qubit (first defined in [2]) to n large Josephson junctions
+    instead of 2. The greatest number of large junctions that has been experimentally
+    realized in a SNAIL qubit is 3. Typically, one assumes
+    :math:`E_{J1}=E_{J2}=E_{J3}=E_J` and :math:`E_{J4}=\alpha E_J`.
+    In the case of 3 Josephson junctions, the Hamiltonian is given by
 
     .. math::
 
@@ -31,8 +37,8 @@ class Snail(QubitBaseClass):
                       -&E_J\cos(\phi_1)-E_J\cos(\phi_2-\phi_1)-E_J\cos(\phi_3-\phi_2) \\
                       -&\alpha E_J\cos(2\pi f-\phi_3),
 
-    where :math:`i,j\in\{1,2,3\}` is represented in the charge basis for the three degrees
-    of freedom. Initialize with, for example::
+    where :math:`i,j\in\{1,2,3\}` is represented in the charge basis for the three
+    degrees of freedom. Initialize with, for example::
 
         EJ = 35.0
         alpha = 0.6
@@ -46,27 +52,28 @@ class Snail(QubitBaseClass):
     EJ1, EJ2, EJ3, EJ4: float
         Josephson energy of the ith junction
         `EJ1 = EJ2 = EJ3`, with `EJ3 = alpha * EJ1` and `alpha <= 1`
-    ECJ1, ECJ2, ECJ3, ECJ4: float
-        charging energy associated with the ith junction
+    EC1, EC2, EC3, EC4: float
+        charging energy associated with the ith junction, may include parallel shunt
+        capacitance
     ECg1, ECg2, ECg3, ECg4: float
-        charging energy associated with the capacative coupling to ground for the
+        charging energy associated with the capacitive coupling to ground for the
         three islands
     ng1, ng2, ng3: float
-        offset charge associated with island i
+        offset charge associated with each island
     flux: float
         magnetic flux through the circuit loop, measured in units of the flux quantum
     ncut: int
-        charge number cutoff for the charge on the three islands `n`, `n = -ncut, ..., ncut`
+        charge number cutoff for the charge `n` on the three islands `n`,
+        `n = -ncut, ..., ncut`
     """
-
     EJ1 = descriptors.WatchedProperty(float, "QUANTUMSYSTEM_UPDATE")
     EJ2 = descriptors.WatchedProperty(float, "QUANTUMSYSTEM_UPDATE")
     EJ3 = descriptors.WatchedProperty(float, "QUANTUMSYSTEM_UPDATE")
     EJ4 = descriptors.WatchedProperty(float, "QUANTUMSYSTEM_UPDATE")
-    ECJ1 = descriptors.WatchedProperty(float, "QUANTUMSYSTEM_UPDATE")
-    ECJ2 = descriptors.WatchedProperty(float, "QUANTUMSYSTEM_UPDATE")
-    ECJ3 = descriptors.WatchedProperty(float, "QUANTUMSYSTEM_UPDATE")
-    ECJ4 = descriptors.WatchedProperty(float, "QUANTUMSYSTEM_UPDATE")
+    EC1 = descriptors.WatchedProperty(float, "QUANTUMSYSTEM_UPDATE")
+    EC2 = descriptors.WatchedProperty(float, "QUANTUMSYSTEM_UPDATE")
+    EC3 = descriptors.WatchedProperty(float, "QUANTUMSYSTEM_UPDATE")
+    EC4 = descriptors.WatchedProperty(float, "QUANTUMSYSTEM_UPDATE")
     ECg1 = descriptors.WatchedProperty(float, "QUANTUMSYSTEM_UPDATE")
     ECg2 = descriptors.WatchedProperty(float, "QUANTUMSYSTEM_UPDATE")
     ECg3 = descriptors.WatchedProperty(float, "QUANTUMSYSTEM_UPDATE")
@@ -82,10 +89,10 @@ class Snail(QubitBaseClass):
         EJ2: float,
         EJ3: float,
         EJ4: float,
-        ECJ1: float,
-        ECJ2: float,
-        ECJ3: float,
-        ECJ4: float,
+        EC1: float,
+        EC2: float,
+        EC3: float,
+        EC4: float,
         ECg1: float,
         ECg2: float,
         ECg3: float,
@@ -98,63 +105,76 @@ class Snail(QubitBaseClass):
         id_str: Optional[str] = None,
     ) -> None:
         base.QuantumSystem.__init__(self, id_str=id_str)
+        # Constant parameters for each Josephson Junction
         self.EJ1 = EJ1
         self.EJ2 = EJ2
         self.EJ3 = EJ3
         self.EJ4 = EJ4
-        self.ECJ1 = ECJ1
-        self.ECJ2 = ECJ2
-        self.ECJ3 = ECJ3
-        self.ECJ4 = ECJ4
+        # Capacitance connected to each Josephson Junction
+        self.EC1 = EC1
+        self.EC2 = EC2
+        self.EC3 = EC3
+        self.EC4 = EC4
+
+        # Capacitance connected to ground
         self.ECg1 = ECg1
         self.ECg2 = ECg2
         self.ECg3 = ECg3
+
+        # offset charges associated with each Josephson Junction
         self.ng1 = ng1
         self.ng2 = ng2
         self.ng3 = ng3
+        # flux
         self.flux = flux
+        # Truncation dimension
         self.ncut = ncut
         self.truncated_dim = truncated_dim
+        self._image_filename = None
 
     @staticmethod
     def default_params() -> Dict[str, Any]:
         return {
-            "EJ1": 1.0,
-            "EJ2": 1.0,
-            "EJ3": 1.0,
-            "EJ4": 0.7,
-            "ECJ1": 1,
-            "ECJ2": 1,
-            "ECJ3": 1,
-            "ECJ4": 1 / 0.7,
-            "ECg1": 10,
-            "ECg2": 10,
-            "ECg3": 10,
+            "EJ1": 887.3,
+            "EJ2": 887.3,
+            "EJ3": 887.3,
+            "EJ4": 117.5,
+            "EC1": 0.2873,
+            "EC2": 0.2873,
+            "EC3": 0.2873,
+            "EC4": 1.437,
+            "ECg1": 193.7,
+            "ECg2": 193.7,
+            "ECg3": 193.7,
             "ng1": 0.0,
             "ng2": 0.0,
             "ng3": 0.0,
-            "flux": 0.41,
-            "ncut": 10,
-            "truncated_dim": 10,
+            "flux": 0.0,
+            "ncut": 6,
+            "truncated_dim": 6,
         }
 
+    # Construct the Ec matrix, we need this to calculate the kinetic_energy matrix in
+    # the Hamiltonian
     def EC_matrix(self) -> ndarray:
-        """Return the charging energy matrix"""
-        CJ1 = 1.0 / (2 * self.ECJ1)  # capacitances in units where e is set to 1
-        CJ2 = 1.0 / (2 * self.ECJ2)
-        CJ3 = 1.0 / (2 * self.ECJ3)
-        CJ4 = 1.0 / (2 * self.ECJ4)
-        Cg1 = 1.0 / (2 * self.ECg1)
-        Cg2 = 1.0 / (2 * self.ECg2)
-        Cg3 = 1.0 / (2 * self.ECg3)
-        Cmat = np.array(
+        """Returns the charging energy matrix"""
+        c1 = 1 / (2 * self.EC1)
+        c2 = 1 / (2 * self.EC2)
+        c3 = 1 / (2 * self.EC3)
+        c4 = 1 / (2 * self.EC4)
+
+        cg1 = 1 / (2 * self.ECg1)
+        cg2 = 1 / (2 * self.ECg2)
+        cg3 = 1 / (2 * self.ECg3)
+        cmat = np.array(
             [
-                [CJ1 + CJ2 + Cg1, -CJ2, 0],
-                [-CJ2, CJ2 + CJ3 + Cg2, -CJ3],
-                [0, -CJ3, CJ3 + CJ4 + Cg3],
+                [c1 + c2 + cg1, -c2, 0],
+                [-c2, c2 + c3 + cg2, -c3],
+                [0, -c3, c3 + c4 + cg3],
             ]
         )
-        return np.linalg.inv(Cmat) / 2.0
+        ec_matrix = 0.5 * np.linalg.inv(cmat)
+        return ec_matrix
 
     def _evals_calc(self, evals_count: int) -> ndarray:
         hamiltonian_mat = self.hamiltonian()
@@ -173,11 +193,11 @@ class Snail(QubitBaseClass):
 
     def hilbertdim(self) -> int:
         """Return Hilbert space dimension."""
-        return (2 * self.n_cut + 1) ** 3
+        return (2 * self.ncut + 1) ** 3
 
     def potential(self, phi1: ndarray, phi2: ndarray, phi3: ndarray) -> ndarray:
-        """Return value of the potential energy at phi1 and phi2 and phi3, disregarding
-        constants."""
+        """Returns the value of the potential energy at phi1 and phi2 and phi3,
+        disregarding constants."""
         return (
             -self.EJ1 * np.cos(phi1)
             - self.EJ2 * np.cos(phi2 - phi1)
@@ -187,36 +207,33 @@ class Snail(QubitBaseClass):
 
     def kineticmat(self) -> ndarray:
         """Return the kinetic energy matrix."""
-        ec = self.EC_matrix()
-        identity = sparse.identity(2 * self.n_cut + 1, format="csc")
+        ec_mat = self.EC_matrix()
+        identity = sparse.identity(2 * self.ncut + 1, format="csc")
 
-        n1 = np.arange(-self.n_cut, self.n_cut + 1, 1)
-        n1 = sparse.diags(n1).tocsc()
+        n_op = np.arange(-self.ncut, self.ncut + 1, 1)
+        n_op = sparse.diags(n_op).tocsc()
+
         n1 = sparse.kron(
-            sparse.kron(n1, identity, format="csc"), identity, format="csc"
+            sparse.kron(n_op, identity, format="csc"), identity, format="csc"
         )
 
-        n2 = np.arange(-self.n_cut, self.n_cut + 1, 1)
-        n2 = sparse.diags(n2).tocsc()
         n2 = sparse.kron(
-            sparse.kron(identity, n2, format="csc"), identity, format="csc"
+            sparse.kron(identity, n_op, format="csc"), identity, format="csc"
         )
 
-        n3 = np.arange(-self.n_cut, self.n_cut + 1, 1)
-        n3 = sparse.diags(n3).tocsc()
         n3 = sparse.kron(
-            sparse.kron(identity, identity, format="csc"), n3, format="csc"
+            sparse.kron(identity, identity, format="csc"), n_op, format="csc"
         )
 
         nvec = np.array([n1, n2, n3])
 
-        return 4 * nvec.T @ ec @ nvec
+        return 4 * nvec.T @ ec_mat @ nvec
 
     def potentialmat(self) -> ndarray:
         """Return the potential energy matrix."""
-        identity = sparse.identity(2 * self.n_cut + 1, format="csc")
+        identity = sparse.identity(2 * self.ncut + 1, format="csc")
 
-        ones_on_diagonal = np.ones((1, 2 * self.n_cut))
+        ones_on_diagonal = np.ones((1, 2 * self.ncut))
         e_positive_phi = sparse.diags(ones_on_diagonal, [1]).tocsc()
         e_negative_phi = sparse.diags(ones_on_diagonal, [-1]).tocsc()
 
