5b8c30d75900261744466a69f2118e4d77465b91
saipavanc <saipavanchitta1998@gmail.com>
2022-08-12 13:02:00 -0500

    Format with Black

--- scqubits/utils/misc.py
+++ scqubits/utils/misc.py
@@ -165,7 +165,9 @@ def check_sync_status(func: Callable) -> Callable:
 def check_sync_status_circuit(func: Callable) -> Callable:
     @functools.wraps(func)
     def wrapper(self, *args, **kwargs):
-        if (not hasattr(self, "parent") and self._is_parameter_updated) or (self.hierarchical_diagonalization and self._out_of_sync):
+        if (not hasattr(self, "parent") and self._is_parameter_updated) or (
+            self.hierarchical_diagonalization and self._out_of_sync
+        ):
             raise Exception(
                 "[scqubits] Circuit or Subsystem parameters have been changed and/or "
                 " the parameter/s in the current instance are is/are out of sync. "
