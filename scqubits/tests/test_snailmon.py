ebc9b6d23ea59ce0e28280fe9fb19a2f8de33b82
matthewcapocci <matthewcapocci98@gmail.com>
2022-09-05 11:20:59 -0500

    Initial Commit

---
+++ scqubits/tests/test_snailmon.py
@@ -0,0 +1,29 @@
+# test_snailmon.py
+# meant to be run with 'pytest'
+#
+# This file is part of scqubits: a Python package for superconducting qubits,
+# Quantum 5, 583 (2021). https://quantum-journal.org/papers/q-2021-11-17-583/
+#
+#    Copyright (c) 2019 and later, Jens Koch and Peter Groszkowski
+#    All rights reserved.
+#
+#    This source code is licensed under the BSD-style license found in the
+#    LICENSE file in the root directory of this source tree.
+############################################################################
+
+import numpy as np
+
+from scqubits import Snailmon
+from scqubits.tests.conftest import StandardTests
+
+
+class TestSnailmon(StandardTests):
+    @classmethod
+    def setup_class(cls):
+        cls.qbt = None
+        cls.qbt_type = Snailmon
+        cls.file_str = "snailmon"
+        cls.op1_str = "n_1_operator"
+        cls.op2_str = "n_2_operator"
+        cls.param_name = "flux"
+        cls.param_list = np.linspace(0.45, 0.55, 50)
