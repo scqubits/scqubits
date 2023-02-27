1760c03c58eba031be56a67d7e13f6f08a3209e3
jkochNU <jens-koch@northwestern.edu>
2023-02-26 18:51:55 -0600

    fix type annotation issue

--- scqubits/ui/hspace_widget.py
+++ scqubits/ui/hspace_widget.py
@@ -466,7 +466,7 @@ class HilbertSpaceUi:
 
     def validated_interact_list(
         self,
-    ) -> Union[Literal[False], List[Union["InteractionTerm", "InteractionTermStr"]]]:
+    ) -> Union[Literal[False], List[Union[scqubits.InteractionTerm, scqubits.InteractionTermStr]]]:
         self.status_output.children = []
 
         main = importlib.import_module("__main__")
