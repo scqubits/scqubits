b6f93c0d7fd9b58673d4d40eb4bd34d964429cc1
jkochNU <jens-koch@northwestern.edu>
2023-03-05 16:26:44 -0600

    scipy deprecation: `linalg.eigh` option `eigvals` -> `subset_by_index`

--- scqubits/core/flux_qubit.py
+++ scqubits/core/flux_qubit.py
@@ -398,14 +398,14 @@ class FluxQubit(base.QubitBaseClass, serializers.Serializable, NoisyFluxQubit):
     def _evals_calc(self, evals_count: int) -> ndarray:
         hamiltonian_mat = self.hamiltonian()
         evals = sp.linalg.eigh(
-            hamiltonian_mat, eigvals=(0, evals_count - 1), eigvals_only=True
+            hamiltonian_mat, subset_by_index=(0, evals_count - 1), eigvals_only=True
         )
         return np.sort(evals)
 
     def _esys_calc(self, evals_count: int) -> Tuple[ndarray, ndarray]:
         hamiltonian_mat = self.hamiltonian()
         evals, evecs = sp.linalg.eigh(
-            hamiltonian_mat, eigvals=(0, evals_count - 1), eigvals_only=False
+            hamiltonian_mat, subset_by_index=(0, evals_count - 1), eigvals_only=False
         )
         evals, evecs = spec_utils.order_eigensystem(evals, evecs)
         return evals, evecs
