d0aae01f8b8fe7409f2b53788f5547bbb0f0a999
Jens Koch <jens-koch@northwestern.edu>
2023-07-27 13:32:45 -0500

    disable eigh nan check

--- scqubits/core/qubit_base.py
+++ scqubits/core/qubit_base.py
@@ -266,14 +266,14 @@ class QubitBaseClass(QuantumSystem, ABC):
     def _evals_calc(self, evals_count: int) -> ndarray:
         hamiltonian_mat = self.hamiltonian()
         evals = sp.linalg.eigh(
-            hamiltonian_mat, eigvals_only=True, subset_by_index=(0, evals_count - 1)
+            hamiltonian_mat, eigvals_only=True, subset_by_index=(0, evals_count - 1), check_finite=False
         )
         return np.sort(evals)
 
     def _esys_calc(self, evals_count: int) -> Tuple[ndarray, ndarray]:
         hamiltonian_mat = self.hamiltonian()
         evals, evecs = sp.linalg.eigh(
-            hamiltonian_mat, eigvals_only=False, subset_by_index=(0, evals_count - 1)
+            hamiltonian_mat, eigvals_only=False, subset_by_index=(0, evals_count - 1), check_finite=False
         )
         evals, evecs = order_eigensystem(evals, evecs)
         return evals, evecs
