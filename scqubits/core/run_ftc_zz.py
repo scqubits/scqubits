3446815567e191dbfd88355cf96e152532c3a646
Danny <dkweiss@u.northwestern.edu>
2023-08-01 21:01:05 -0400

    updated run file

--- scqubits/core/run_ftc_zz.py
+++ scqubits/core/run_ftc_zz.py
@@ -1,4 +1,5 @@
 import argparse
+from itertools import product
 
 import numpy as np
 import h5py
@@ -27,33 +28,59 @@ FTC_grounded = scq.FluxoniumTunableCouplerGrounded(EJa=E_Ja, EJb=E_Jb, EC_twoqub
                                                fluxonium_minus_truncated_dim=7, h_o_truncated_dim=7)
 
 
-def run_ftc_zz(flux_a, flux_b, flux_c, highest_exc_q=2, highest_exc_m=3, highest_exc_p=3):
+def run_ftc_zz(
+    idx,
+    num_pts,
+    num_cpus=4,
+    highest_exc_q=2,
+    highest_exc_m=3,
+    highest_exc_p=3,
+    flux_q_max=0.06,
+    flux_c_max=0.5,
+):
+    flux_q_linspace = np.linspace(0.0, flux_q_max, num_pts)
+    flux_c_linspace = np.linspace(0.0, flux_c_max, num_pts)
+    flux_prod = list(product(flux_q_linspace, flux_c_linspace))
+    flux_q, flux_c = flux_prod[idx]
     FTC_grounded.flux_c = flux_c
     (E00, E00_4, E_00_2) = FTC_grounded.fourth_order_energy_shift(
-        0, 0, flux_a=flux_a, flux_b=flux_b, highest_exc_q=highest_exc_q, highest_exc_m=highest_exc_m,
-        highest_exc_p=highest_exc_p, num_cpus=8)
+        0, 0, flux_a=0.5 + flux_q, flux_b=0.5 - flux_q, highest_exc_q=highest_exc_q, highest_exc_m=highest_exc_m,
+        highest_exc_p=highest_exc_p, num_cpus=num_cpus)
     (E01, E01_4, E_01_2) = FTC_grounded.fourth_order_energy_shift(
-        0, 1, flux_a=flux_a, flux_b=flux_b, highest_exc_q=highest_exc_q, highest_exc_m=highest_exc_m,
-        highest_exc_p=highest_exc_p, num_cpus=8)
+        0, 1, flux_a=0.5 + flux_q, flux_b=0.5 - flux_q, highest_exc_q=highest_exc_q, highest_exc_m=highest_exc_m,
+        highest_exc_p=highest_exc_p, num_cpus=num_cpus)
     (E10, E10_4, E_10_2) = FTC_grounded.fourth_order_energy_shift(
-        1, 0, flux_a=flux_a, flux_b=flux_b, highest_exc_q=highest_exc_q, highest_exc_m=highest_exc_m,
-        highest_exc_p=highest_exc_p, num_cpus=8)
+        1, 0, flux_a=0.5 + flux_q, flux_b=0.5 - flux_q, highest_exc_q=highest_exc_q, highest_exc_m=highest_exc_m,
+        highest_exc_p=highest_exc_p, num_cpus=num_cpus)
     (E11, E11_4, E_11_2) = FTC_grounded.fourth_order_energy_shift(
-        1, 1, flux_a=flux_a, flux_b=flux_b, highest_exc_q=highest_exc_q, highest_exc_m=highest_exc_m,
-        highest_exc_p=highest_exc_p, num_cpus=8)
-    return E11 - E10 - E01 + E00
+        1, 1, flux_a=0.5 + flux_q, flux_b=0.5 - flux_q, highest_exc_q=highest_exc_q, highest_exc_m=highest_exc_m,
+        highest_exc_p=highest_exc_p, num_cpus=num_cpus)
+    data_dict = {"eta": E11 - E10 - E01 + E00}
+    param_dict = {"highest_exc_q": highest_exc_q, "highest_exc_m": highest_exc_m, "highest_exc_p": highest_exc_p,
+                  "flux_q_max": flux_q_max, "flux_c_max": flux_c_max}
+    filepath = str(idx).zfill(5)+"ZZ_shift_vs_flux.h5py"
+    write_to_h5(filepath, data_dict, param_dict)
+
+
+def write_to_h5(filepath, data_dict, param_dict, loud=True):
+    if loud:
+        print(f"writing data to {filepath}")
+    with h5py.File(filepath, "w") as f:
+        for key, val in data_dict.items():
+            written_data = f.create_dataset(key, data=val)
+        for kwarg in param_dict.keys():
+            try:
+                f.attrs[kwarg] = param_dict[kwarg]
+            except TypeError:
+                f.attrs[kwarg] = str(param_dict[kwarg])
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="ftc zz")
-    parser.add_argument(
-        "--dev_amount", default="0.0", type=str, help="gamma deviation in GHz"
-    )
     parser.add_argument("--idx", default=0, type=int, help="index that is unraveled")
     parser.add_argument(
-        "--num_pts", default=5, type=int, help="number of points in gamma_dev_list"
+        "--num_pts", default=5, type=int, help="number of points in flux lists"
     )
     parser.add_argument("--num_cpus", default=8, type=int, help="num cpus")
     args = parser.parse_args()
-    run_gamma_dev(
-        float(args.dev_amount), args.idx, args.num_pts, num_cpus=args.num_cpus
-    )
+    run_ftc_zz(args.idx, args.num_pts, num_cpus=args.num_cpus)
