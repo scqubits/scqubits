fc5b00b8bddf2f8e00df143c30eb3505c55099fd
Danyang Chen <danyangchen2026@u.northwestern.edu>
2023-08-23 14:04:31 -0500

    small fix for type var annotation

--- scqubits/core/qubit_base.py
+++ scqubits/core/qubit_base.py
@@ -79,7 +79,7 @@ Transition = Tuple[int, int]
 TransitionsTuple = Tuple[Transition, ...]
 
 # annotate the types will inherit from Serializable
-QubitType = TypeVar("QubitType", bound="QuantumSystem")
+QuantumSystemType = TypeVar("QuantumSystemType", bound="QuantumSystem")
 
 # -Generic quantum system container and Qubit base class------------------------------
 
@@ -101,7 +101,7 @@ class QuantumSystem(DispatchClient, ABC):
 
     _subclasses: List[ABCMeta] = []
 
-    def __new__(cls: Type[QubitType], *args, **kwargs) -> QubitType:
+    def __new__(cls: Type[QuantumSystemType], *args, **kwargs) -> QuantumSystemType:
         QuantumSystem._quantumsystem_counter += 1
 
         if cls.__name__ not in QuantumSystem._instance_counter:
