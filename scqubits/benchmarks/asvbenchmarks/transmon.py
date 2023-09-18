a061be517db7869ea34a7584844af7b0ad812f6a
yellowbow <kchen4000@gmail.com>
2023-09-18 18:00:44 -0500

    added fluxonium

--- scqubits/benchmarks/asvbenchmarks/transmon.py
+++ scqubits/benchmarks/asvbenchmarks/transmon.py
@@ -1,13 +1,20 @@
 import numpy as np
 from scqubits import Transmon
+from scqubits import Fluxonium
 
 # from scqubits.benchmarks.asvconftest import StandardTests
 # import scqubits.benchmarks.asvconftest as asvtest
 
 
-class TestTransmon:
+class TestQubit:
 
-    def time_get_spectrum_vs_paramvals(self):
+    def time_transmon_get_spectrum_vs_paramvals(self):
         CPB = Transmon.create()
-        ng_list = np.linspace(-2, 2, 50)
+        ng_list = np.linspace(-2, 2, 100)
         return CPB.get_spectrum_vs_paramvals('ng', ng_list, evals_count=4, subtract_ground=False, get_eigenstates=True)
+    
+    def time_fluxonium_get_spectrum_vs_paramvals(self):
+        qbt = Fluxonium.create()
+        flux_list = np.linspace(-0.5, 0.5, 51)
+        return qbt.get_spectrum_vs_paramvals('flux', flux_list, evals_count=4, subtract_ground=False, get_eigenstates=True)
+    
