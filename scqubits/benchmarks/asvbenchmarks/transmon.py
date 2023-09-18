6d7271a3986f29f482d314f3aa91e03c94d16d23
yellowbow <kchen4000@gmail.com>
2023-09-18 17:18:34 -0500

    changed cls to self

--- scqubits/benchmarks/asvbenchmarks/transmon.py
+++ scqubits/benchmarks/asvbenchmarks/transmon.py
@@ -8,15 +8,14 @@ from scqubits.benchmarks.asvconftest import StandardTests
 
 
 class TestTransmon(StandardTests):
-    @classmethod
-    def setup_class(cls):
-        cls.qbt = None
-        cls.qbt_type = Transmon
-        cls.file_str = "transmon"
-        cls.op1_str = "n_operator"
-        cls.op2_str = "n_operator"
-        cls.param_name = "ng"
-        cls.param_list = np.linspace(-1, 1, 100)
+    def setup_class(self):
+        self.qbt = None
+        self.qbt_type = Transmon
+        self.file_str = "transmon"
+        self.op1_str = "n_operator"
+        self.op2_str = "n_operator"
+        self.param_name = "ng"
+        self.param_list = np.linspace(-1, 1, 100)
 
     def time_plot_n_wavefunction(self):
         self.qbt = Transmon(EJ=1.0, EC=1.0, ng=0.0, ncut=10)
