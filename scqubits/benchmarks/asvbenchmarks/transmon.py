4f0efe49cc6279f1323ce207d2cde0a82eed7579
yellowbow <kchen4000@gmail.com>
2023-09-18 18:03:34 -0500

    commented

--- scqubits/benchmarks/asvbenchmarks/transmon.py
+++ scqubits/benchmarks/asvbenchmarks/transmon.py
@@ -15,6 +15,6 @@ class TestQubit:
     
     def time_fluxonium_get_spectrum_vs_paramvals(self):
         qbt = Fluxonium.create()
-        flux_list = np.linspace(-0.5, 0.5, 51)
-        return qbt.get_spectrum_vs_paramvals('flux', flux_list, evals_count=4, subtract_ground=False, get_eigenstates=True)
+        # flux_list = np.linspace(-0.5, 0.5, 51)
+        # return qbt.get_spectrum_vs_paramvals('flux', flux_list, evals_count=4, subtract_ground=False, get_eigenstates=True)
     
