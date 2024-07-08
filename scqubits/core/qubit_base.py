adfcb685472da21947b821f9fc646f382da854fc
Charlie Schlicker <CharlesSchlicker2025@u.northwestern.edu>
2024-07-08 14:26:18 -0700

    test

--- scqubits/core/qubit_base.py
+++ scqubits/core/qubit_base.py
@@ -635,7 +635,7 @@ class QubitBaseClass(QuantumSystem, ABC):
         target_map = get_map_method(num_cpus)
 
         # Exploitation of periodicity in ng values 
-        using = True   # For testing
+        using = False   # For testing
         from transmon import Transmon, TunableTransmon
         # Check class is transmon and the parameter is ng
         if isinstance(self, Transmon) and param_name == 'ng' and using:
