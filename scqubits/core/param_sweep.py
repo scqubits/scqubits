ec9073e65498de616617b29a1e0c175520037af0
Danyang Chen <danyangchen2026@u.northwestern.edu>
2024-11-17 18:34:42 -0600

    remove the redundant _preslicing_reset

--- scqubits/core/param_sweep.py
+++ scqubits/core/param_sweep.py
@@ -256,7 +256,7 @@ class ParameterSweepBase(ABC, SpectrumLookupMixin):
                     param_vals=self._parameters[sweep_param_name],
                 )
             )
-        self._preslicing_reset()
+        self.reset_preslicing()
         return specdata_list
 
     @property
@@ -284,7 +284,7 @@ class ParameterSweepBase(ABC, SpectrumLookupMixin):
             param_name=sweep_param_name,
             param_vals=self._parameters[sweep_param_name],
         )
-        self._preslicing_reset()
+        self.reset_preslicing()
         return specdata
 
     def get_sweep_indices(self, multi_index: GIndexTuple) -> List[int]:
@@ -302,17 +302,12 @@ class ParameterSweepBase(ABC, SpectrumLookupMixin):
                 (list, tuple, ndarray),
             )
         ]
-        self._preslicing_reset()
+        self.reset_preslicing()
         return sweep_indices
 
     @property
     def system_params(self) -> Dict[str, Any]:
         return self.hilbertspace.get_initdata()
-
-    def _preslicing_reset(self) -> None:
-        self._current_param_indices = (
-            (slice(None, None, None),) * len(self._parameters)
-        )
 
     def _slice_is_1d_sweep(self, param_indices: Optional[NpIndices]) -> bool:
         param_indices = param_indices or self._current_param_indices
@@ -616,7 +611,7 @@ class ParameterSweepBase(ABC, SpectrumLookupMixin):
                         transitions.append((initial_state, final_state))
                         transition_energies.append(diff_energies)
 
-        self._preslicing_reset()
+        self.reset_preslicing()
 
         if not as_specdata:
             return transitions, transition_energies
@@ -794,7 +789,7 @@ class ParameterSweepBase(ABC, SpectrumLookupMixin):
         if make_positive:
             specdata_all.energy_table = np.abs(specdata_all.energy_table)
 
-        self._preslicing_reset()
+        self.reset_preslicing()
 
         if coloring == "plain":
             return specdata_all.plot_evals_vs_paramvals()
@@ -1020,7 +1015,7 @@ class ParameterSweep(  # type:ignore
         self._num_cpus = num_cpus
 
         self._out_of_sync = False
-        self._preslicing_reset()
+        self.reset_preslicing()
 
         dispatch.CENTRAL_DISPATCH.register("PARAMETERSWEEP_UPDATE", self)
         dispatch.CENTRAL_DISPATCH.register("HILBERTSPACE_UPDATE", self)
